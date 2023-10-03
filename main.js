1063421c1917194d57fa765ebb7199633b77ee09
Mohammed Fakih <mhd.fakih@gmail.com>
2023-10-03 15:57:27 +0300

    cleared js file from junk code

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-setTimeout(() => {
-    console.info(`first log after 1s`);
-}, 1000);
-setTimeout(() => {
-    console.table(`second log after 2s`);
-}, 2000);
-setTimeout(() => {
-    console.error(`third log after 3s`);
-}, 3000);
-setTimeout(() => {
-    console.warn(`forth log after 4s`);
-}, 4000);
+"use strict";
