3bf6772811539571eeb9da1f1204ff8054e4edfd
Mohammed Fakih <mhd.fakih@gmail.com>
2023-10-06 00:17:46 +0300

    javascript part is done

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const inputEl = document.getElementById("toggle");
+
+const basicMonthlyPrice = document.getElementById("basic-monthly");
+const basicYearlyPrice = document.getElementById("basic-yearly");
+
+const professionalMonthlyPrice = document.getElementById(
+  "professional-monthly"
+);
+const professionalYearlyPrice = document.getElementById("professional-yearly");
+
+const masterMonthlyPrice = document.getElementById("master-monthly");
+const masterYearlyPrice = document.getElementById("master-yearly");
+
+function updatePrice() {
+  if (inputEl.checked) {
+    basicMonthlyPrice.classList.remove("hidden");
+    basicYearlyPrice.classList.add("hidden");
+    professionalMonthlyPrice.classList.remove("hidden");
+    professionalYearlyPrice.classList.add("hidden");
+    masterMonthlyPrice.classList.remove("hidden");
+    masterYearlyPrice.classList.add("hidden");
+  } else {
+    basicMonthlyPrice.classList.add("hidden");
+    basicYearlyPrice.classList.remove("hidden");
+    professionalMonthlyPrice.classList.add("hidden");
+    professionalYearlyPrice.classList.remove("hidden");
+    masterMonthlyPrice.classList.add("hidden");
+    masterYearlyPrice.classList.remove("hidden");
+  }
+}
+
+inputEl.addEventListener("change", updatePrice);
